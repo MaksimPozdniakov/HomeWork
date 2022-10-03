794e4f52b5677d832db7fbcab1f9f215f69740e5
Maksim <p.m.pavlovich@ya.ru>
2022-10-03 18:37:39 +0300

    added 2 and 3 tasks

--- HomeWork_001/Program.cs
+++ HomeWork_001/Program.cs
@@ -22,3 +22,34 @@ Console.WriteLine("max = " + max);
 Console.WriteLine("min = " + min);
 */
 
+//Задача №2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.WriteLine("Input first num ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second num ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third num ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if(n2 > max) max = n2;
+if(n3 > max) max = n3;
+
+Console.WriteLine("max = " + max);
+*/
+
+//Задача №3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ");
+}
+*/
