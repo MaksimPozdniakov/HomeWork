252c513d82a633282b16853a4d7551720fe8203f
Maksim <p.m.pavlovich@ya.ru>
2022-10-04 14:28:58 +0300

    added 4 task

--- HomeWork_001/Program.cs
+++ HomeWork_001/Program.cs
@@ -53,3 +53,28 @@ else
     Console.WriteLine("НЕТ");
 }
 */
+
+//Задача №4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current;
+if (number > 0)
+{
+    current = 1;
+}
+else
+{
+    current = number;
+}
+while (current <= number)
+{
+    if (current % 2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+
+    current++;
+}
+*/
