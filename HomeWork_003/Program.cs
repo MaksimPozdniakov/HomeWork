6cd7d4cb759c060a1baca6215d2eccc243b14fcb
Maksim <p.m.pavlovich@ya.ru>
2022-10-11 18:57:41 +0300

    completed tasks for HomeWork_003

--- HomeWork_003/Program.cs
+++ HomeWork_003/Program.cs
@@ -8,25 +8,80 @@ void Palindrom(int num)
     
     if(num < 10000 | num > 99999)
     {
-        Console.WriteLine(" ggg ");
+        Console.WriteLine("Данное число не является пятизначным ");
     }
     
     else
     {
         if (firstNum == lastNum)
         {
-            firstNum = firstNum / (10000/10);
-            lastNum %= 10;
+            firstNum = firstNum / 1000 % 10;
+            lastNum = lastNum % 100 / 10;
+            
+            Console.WriteLine("Данное число является палиндромом ");
         }
-    
+        else
+        {
+            Console.WriteLine("Данное число не является палиндромом ");
+        }
+            
+        
     }
-    
 }
 
 Console.Write("Input number ");
 int a = Convert.ToInt32(Console.ReadLine());
-
 Palindrom(a);
 */
+// Задача №2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+double Rastoynie(double xA, double xB, double xC, double yA, double yB, double yC)
+{
+    double abc;
+    abc = Math.Sqrt(Math.Pow(xC-xB-xA,2)+Math.Pow(yC-yB-yA,2));
+    return Math.Round(abc, 2);
+}
+Console.Write("Input xA ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input xB ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yA ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yB ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yA ");
+double xC = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yB ");
+double yC = Convert.ToDouble(Console.ReadLine());
+Console.Write(Rastoynie(xA, xB, xC, yA, yB, yC));
+*/
+// Задача №3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+int TablCubs(int num)
+{
+    int current;
+    
+    if (num > 0)
+    {
+        current = 1;
+    }   
+    
+    else
+    {
+        current = num;
+    }
+    
+    while (current <= num)
+    {
+        Console.Write(Math.Pow(current, 3) + " ");
+       
+        current++;
+    }
+    return current;
+}
 
+Console.Write("Input number ");
+int a = Convert.ToInt32(Console.ReadLine());
+TablCubs(a);
+*/
 
