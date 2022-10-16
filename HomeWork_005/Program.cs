3f6cc72d5e0cc6fbebf0707382819256557e51a5
Maksim <p.m.pavlovich@ya.ru>
2022-10-16 20:37:26 +0300

    Completed first task to HomeWork_005

---
+++ HomeWork_005/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача №1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] NewArray(int size)
+{
+    int[] Array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(100,999); 
+    }
+    Console.WriteLine();
+    return Array;
+}
+
+void PrintMyArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    Console.Write(Array[i] + " ");
+}
+
+int PozitivNumbers(int[] Array)
+{
+    int count = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Imput numbers of array ");
+int size = Convert.ToInt32(Console.ReadLine());
+PrintMyArray(NewArray(size));
+
+int[] myArray = NewArray(size);
+int result = PozitivNumbers(myArray);
+
+Console.WriteLine($"Pozitiv numbers is {result}");
+*/
