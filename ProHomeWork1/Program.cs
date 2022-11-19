46d6cd6926fcb62091885bd24a8425dcf5f35fb2
Maksim <p.m.pavlovich@ya.ru>
2022-11-19 23:51:10 +0300

    added new folder ProHomeWork1

---
+++ ProHomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿// таблица умножения
+/*
+void Table(int num)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            int result = j * i;
+            Console.Write($"{i} * {j} = {result} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int num = 1;
+Table(num);
+*/
+
